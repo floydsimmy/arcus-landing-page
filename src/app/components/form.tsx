21b7552213c74bc907acdc5e92876509400b8093
Claudio Tomas <ccaducarlos@gmail.com>
2024-09-19 07:55:55 +0200

    feat: add form

---
+++ src/app/components/form.tsx
@@ -0,0 +1,72 @@
+"use client";
+import { Input } from "@/app/components/input";
+import { Label } from "@/app/components/label";
+import { cn } from "@/lib/utils";
+import React from "react";
+import { Button } from "./button";
+import { TextArea } from "./textarea";
+
+export function Form() {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Form submitted");
+  };
+  return (
+    <div className="w-full  rounded-none md:rounded-2xl bg-white ">
+      <div className="flex flex-col gap-3">
+        <h2 className="text-4xl leading-[3.3rem] font-medium text-slate-600">Contacte-nos</h2>
+        <p className="leading-7 text-lg">Etapas para a execução dos nossos projectos</p>
+      </div>
+
+      <form className="my-8" onSubmit={handleSubmit}>
+        <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+          <LabelInputContainer>
+            <Label htmlFor="name">Nome</Label>
+            <Input id="name" placeholder="Insira seu nome" type="text" />
+          </LabelInputContainer>
+        </div>
+        <LabelInputContainer className="mb-4">
+          <Label htmlFor="email">Email</Label>
+          <Input id="email" placeholder="Insira seu email" type="email" />
+        </LabelInputContainer>
+        <Label htmlFor="message">Message</Label>
+
+        <LabelInputContainer className="mb-4">
+
+          <TextArea id="message"
+            placeholder="Write your message here" className="w-full h-[4.375rem]" />
+        </LabelInputContainer>
+
+
+        <LabelInputContainer className="mb-4">
+
+          <Button content="Submeter" type="submit" className="w-full text-center" />
+        </LabelInputContainer>
+
+      </form>
+    </div>
+  );
+}
+
+const BottomGradient = () => {
+  return (
+    <>
+      <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent" />
+      <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
+    </>
+  );
+};
+
+const LabelInputContainer = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div className={cn("flex flex-col space-y-2 w-full", className)}>
+      {children}
+    </div>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,8 @@ import { ProcessCard } from "./components/process-card";
 import { SectionHeader } from "./components/section-header";
 import { ServiceCard } from "./components/service-card";
 import { TestimonialCard } from "./components/testimonial-card";
+import { Form } from "./components/form";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -18,108 +20,135 @@ export default function Home() {
       </div>
 
       <main className="mt-11 max-w-7xl flex flex-col gap-32 ">
-        <div className="bg-white rounded-[64px] h-[53.75rem] p-16 flex items-center" style={{ backgroundImage: "url('./back.png')", backgroundSize: 'cover', backgroundPosition: 'center' }}>
-
-          <div className="max-w-[40rem] flex flex-col gap-3 text-white">
-            <div className="flex items-center gap-3">
-              <hr className="w-28 border-t-[3px] border-slate-arc-400 " />
-              <span>Arquitectura</span>
-            </div>
-            <h1 className="text-6xl font-bold">Construindo o futuro com solidez, confiança e inovação.</h1>
-            <p className="leading-8">
-              Transformamos projetos em realidade, com dedicação e excelência em cada detalhe. Seja para grandes obras ou pequenas reformas, estamos prontos para concretizar suas ideias.
-            </p>
-            <div className="flex gap-3">
-              <Button variant content="Peça a cotação do seu projeto" />
-              <Button icon={<Phone weight="fill" />} content="Ligue-nos já" />
-
-            </div>
-          </div>
-        </div>
-
-        <div className="flex gap-6 mt-28 ">
-          <InsightCard imgSrc="/list.svg" title="Projectos" text="+100" />
-          <InsightCard imgSrc="/people.svg" title="Trabalhadores" text="+50" />
-          <InsightCard imgSrc="/house.svg" title="Obras concluídas" text="+50" />
-          <InsightCard imgSrc="/person.svg" title="Clientes" text="+100" />
-        </div>
-
-        <div className="flex flex-col gap-10 ">
-          <SectionHeader title="Serviços" description="Oferecemos serviços onde o nosso maior valor é a qualidade e a satisfação do cliente " />
-          <div className="flex gap-6">
-            <ServiceCard imgSrc="/map.svg" title="Arquitectura" text="Nosso serviço de arquitetura combina criatividade e funcionalidade para dar vida a projetos únicos e personalizados." />
-            <ServiceCard imgSrc="/map.svg" title="Planeamento Físico/Urbano" text="Nosso serviço de arquitetura combina criatividade e funcionalidade para dar vida a projetos únicos e personalizados." />
-            <ServiceCard imgSrc="/map.svg" title="Engenharia Multidisciplinar" text="Nosso serviço de arquitetura combina criatividade e funcionalidade para dar vida a projetos únicos e personalizados." />
-            <ServiceCard imgSrc="/map.svg" title="Fiscalização de obras" text="Nosso serviço de arquitetura combina criatividade e funcionalidade para dar vida a projetos únicos e personalizados." />
-
-          </div>
-        </div>
-
-        <div className="flex flex-col gap-10 ">
-          <SectionHeader title="Projectos" description="Oferecemos serviços onde o nosso maior valor é a qualidade e a satisfação do cliente " />
-          <div className="grid grid-cols-2 gap-4">
-            <div className="grid grid-rows-2 gap-4">
-              <div className="row-span-2">
-                <img src="/project-card.png" alt="Merged" />
-              </div>
-            </div>
-            <div className="grid grid-rows-2 gap-4">
-              <div>
-                <img src="/project-card1.png" alt="2" />
-              </div>
-              <div>
-                <img src="/project-card2.png" alt="3" />
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="flex flex-col gap-10 ">
-          <SectionHeader title="Parceiros" description="Oferecemos serviços onde o nosso maior valor é a qualidade e a satisfação do cliente " />
-          <Marquee >
-            <PartnerCard imgSrc="/R.svg" title="Tzu Chi" />
-            <PartnerCard imgSrc="/Revimo.svg" title="Revimo" />
-            <PartnerCard imgSrc="/Julen.svg" title="Julen Construções " />
-            <PartnerCard imgSrc="/UH.svg" title="Un Habit" />
-            <PartnerCard imgSrc="/Petromoc.svg" title="Petromoc" />
-            <PartnerCard imgSrc="/EDM.svg" title="Electricidade de Moçambique" />
-            <PartnerCard imgSrc="/WB.svg" title="World Bank Group" />
-          </Marquee>
-        </div>
-
-        <div className="flex flex-col gap-10 ">
-          <SectionHeader title="Processos" description="Etapas para a execução dos nossos projectos" />
-          <div className="flex gap-6">
-            <ProcessCard imgSrc="/Rectangle1.svg" title="1. Analise Geral" text="Fazemos a analise do seu projecto" />
-            <ProcessCard imgSrc="/Rectangle2.svg" title="2. Assinatura de Contrato" text="Fazemos o levantamento dos requisitos e entramos em um acordo." />
-            <ProcessCard imgSrc="/Rectangle3.svg" title="3. Planeamento do projecto" text="Fazemos o plano de execução do projecto" />
-            <ProcessCard imgSrc="/Rectangle4.svg" title="4. Design da estrutura" text="Vamos a elaboração do design." />
-          </div>
-        </div>
-
-        <div className="flex flex-col gap-10 ">
-          <SectionHeader title="Testemunhos" />
-          <div className="flex flex-col gap-6">
-            <Marquee className="[--duration:10s]">
-             <TestimonialCard title="John Doe" text="A Empresa tem uma qualidade inemaginavel. Nunca antes vista no mercado de arquitectura"/>
-             <TestimonialCard title="John Doe" text="A Empresa tem uma qualidade inemaginavel. Nunca antes vista no mercado de arquitectura"/>
-             <TestimonialCard title="John Doe" text="A Empresa tem uma qualidade inemaginavel. Nunca antes vista no mercado de arquitectura"/>
-             <TestimonialCard title="John Doe" text="A Empresa tem uma qualidade inemaginavel. Nunca antes vista no mercado de arquitectura"/>
-             <TestimonialCard title="John Doe" text="A Empresa tem uma qualidade inemaginavel. Nunca antes vista no mercado de arquitectura"/>
-            </Marquee>
-            <Marquee reverse>
-            <TestimonialCard title="John Doe" className="w-[23.8rem]" text="A Empresa tem uma qualidade inemaginavel. Nunca antes vista no mercado de arquitectura"/>
-             <TestimonialCard title="John Doe" className="w-[23.8rem]" text="A Empresa tem uma qualidade inemaginavel. Nunca antes vista no mercado de arquitectura"/>
-             <TestimonialCard title="John Doe" className="w-[23.8rem]" text="A Empresa tem uma qualidade inemaginavel. Nunca antes vista no mercado de arquitectura"/>
-             <TestimonialCard title="John Doe" className="w-[23.8rem]" text="A Empresa tem uma qualidade inemaginavel. Nunca antes vista no mercado de arquitectura"/>
-             <TestimonialCard title="John Doe" className="w-[23.8rem]" text="A Empresa tem uma qualidade inemaginavel. Nunca antes vista no mercado de arquitectura"/>
-            </Marquee>
-
-            {/* <MarqueeDemo/> */}
-          </div>
-        </div>
+        <HeroSection />
+        <InsightsSection />
+        <ServicesSection />
+        <ProjectsSection />
+        <PartnersSection />
+        <ProcessesSection />
+        <TestimonialsSection />
+        <ContactSection/>
       </main>
     </div>
-
   );
 }
+
+const HeroSection = () => (
+  <div className="bg-white rounded-[64px] h-[53.75rem] p-16 flex items-center" style={{ backgroundImage: "url('./back.png')", backgroundSize: 'cover', backgroundPosition: 'center' }}>
+    <div className="max-w-[40rem] flex flex-col gap-3 text-white">
+      <div className="flex items-center gap-3">
+        <hr className="w-28 border-t-[3px] border-slate-arc-400 " />
+        <span>Arquitectura</span>
+      </div>
+      <h1 className="text-6xl font-bold">Construindo o futuro com solidez, confiança e inovação.</h1>
+      <p className="leading-8">
+        Transformamos projetos em realidade, com dedicação e excelência em cada detalhe. Seja para grandes obras ou pequenas reformas, estamos prontos para concretizar suas ideias.
+      </p>
+      <div className="flex gap-3">
+        <Button variant content="Peça a cotação do seu projeto" />
+        <Button icon={<Phone weight="fill" />} content="Ligue-nos já" />
+      </div>
+    </div>
+  </div>
+);
+
+const InsightsSection = () => (
+  <div className="flex gap-6 mt-28 ">
+    <InsightCard imgSrc="/list.svg" title="Projectos" text="+100" />
+    <InsightCard imgSrc="/people.svg" title="Trabalhadores" text="+50" />
+    <InsightCard imgSrc="/house.svg" title="Obras concluídas" text="+50" />
+    <InsightCard imgSrc="/person.svg" title="Clientes" text="+100" />
+  </div>
+);
+
+const ServicesSection = () => (
+  <div className="flex flex-col gap-10 ">
+    <SectionHeader title="Serviços" description="Oferecemos serviços onde o nosso maior valor é a qualidade e a satisfação do cliente " />
+    <div className="flex gap-6">
+      <ServiceCard imgSrc="/map.svg" title="Arquitectura" text="Nosso serviço de arquitetura combina criatividade e funcionalidade para dar vida a projetos únicos e personalizados." />
+      <ServiceCard imgSrc="/map.svg" title="Planeamento Físico/Urbano" text="Nosso serviço de arquitetura combina criatividade e funcionalidade para dar vida a projetos únicos e personalizados." />
+      <ServiceCard imgSrc="/map.svg" title="Engenharia Multidisciplinar" text="Nosso serviço de arquitetura combina criatividade e funcionalidade para dar vida a projetos únicos e personalizados." />
+      <ServiceCard imgSrc="/map.svg" title="Fiscalização de obras" text="Nosso serviço de arquitetura combina criatividade e funcionalidade para dar vida a projetos únicos e personalizados." />
+    </div>
+  </div>
+);
+
+const ProjectsSection = () => (
+  <div className="flex flex-col gap-10 ">
+    <SectionHeader title="Projectos" description="Oferecemos serviços onde o nosso maior valor é a qualidade e a satisfação do cliente " />
+    <div className="grid grid-cols-2 gap-4">
+      <div className="grid grid-rows-2 gap-4">
+        <div className="row-span-2">
+          <img src="/project-card.png" alt="Merged" />
+        </div>
+      </div>
+      <div className="grid grid-rows-2 gap-4">
+        <div>
+          <img src="/project-card1.png" alt="2" />
+        </div>
+        <div>
+          <img src="/project-card2.png" alt="3" />
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+const PartnersSection = () => (
+  <div className="flex flex-col gap-10 ">
+    <SectionHeader title="Parceiros" description="Oferecemos serviços onde o nosso maior valor é a qualidade e a satisfação do cliente " />
+    <Marquee>
+      <PartnerCard imgSrc="/R.svg" title="Tzu Chi" />
+      <PartnerCard imgSrc="/Revimo.svg" title="Revimo" />
+      <PartnerCard imgSrc="/Julen.svg" title="Julen Construções " />
+      <PartnerCard imgSrc="/UH.svg" title="Un Habit" />
+      <PartnerCard imgSrc="/Petromoc.svg" title="Petromoc" />
+      <PartnerCard imgSrc="/EDM.svg" title="Electricidade de Moçambique" />
+      <PartnerCard imgSrc="/WB.svg" title="World Bank Group" />
+    </Marquee>
+  </div>
+);
+
+const ProcessesSection = () => (
+  <div className="flex flex-col gap-10 ">
+    <SectionHeader title="Processos" description="Etapas para a execução dos nossos projectos" />
+    <div className="flex gap-6">
+      <ProcessCard imgSrc="/Rectangle1.svg" title="1. Analise Geral" text="Fazemos a analise do seu projecto" />
+      <ProcessCard imgSrc="/Rectangle2.svg" title="2. Assinatura de Contrato" text="Fazemos o levantamento dos requisitos e entramos em um acordo." />
+      <ProcessCard imgSrc="/Rectangle3.svg" title="3. Planeamento do projecto" text="Fazemos o plano de execução do projecto" />
+      <ProcessCard imgSrc="/Rectangle4.svg" title="4. Design da estrutura" text="Vamos a elaboração do design." />
+    </div>
+  </div>
+);
+
+const TestimonialsSection = () => (
+  <div className="flex flex-col gap-10 ">
+    <SectionHeader title="Testemunhos" />
+    <div className="flex flex-col gap-6">
+      <Marquee className="[--duration:10s]">
+        <TestimonialCard title="John Doe" text="A Empresa tem uma qualidade inemaginavel. Nunca antes vista no mercado de arquitectura" />
+        <TestimonialCard title="John Doe" text="A Empresa tem uma qualidade inemaginavel. Nunca antes vista no mercado de arquitectura" />
+        <TestimonialCard title="John Doe" text="A Empresa tem uma qualidade inemaginavel. Nunca antes vista no mercado de arquitectura" />
+        <TestimonialCard title="John Doe" text="A Empresa tem uma qualidade inemaginavel. Nunca antes vista no mercado de arquitectura" />
+        <TestimonialCard title="John Doe" text="A Empresa tem uma qualidade inemaginavel. Nunca antes vista no mercado de arquitectura" />
+      </Marquee>
+      <Marquee reverse>
+        <TestimonialCard title="John Doe" className="w-[23.8rem]" text="A Empresa tem uma qualidade inemaginavel. Nunca antes vista no mercado de arquitectura" />
+        <TestimonialCard title="John Doe" className="w-[23.8rem]" text="A Empresa tem uma qualidade inemaginavel. Nunca antes vista no mercado de arquitectura" />
+        <TestimonialCard title="John Doe" className="w-[23.8rem]" text="A Empresa tem uma qualidade inemaginavel. Nunca antes vista no mercado de arquitectura" />
+        <TestimonialCard title="John Doe" className="w-[23.8rem]" text="A Empresa tem uma qualidade inemaginavel. Nunca antes vista no mercado de arquitectura" />
+        <TestimonialCard title="John Doe" className="w-[23.8rem]" text="A Empresa tem uma qualidade inemaginavel. Nunca antes vista no mercado de arquitectura" />
+      </Marquee>
+    </div>
+  </div>
+);
+
+const ContactSection = () => (
+  <div className="flex flex-col gap-10 ">
+    
+    <div className="grid grid-cols-2 gap-6">
+      <Form />
+      <Image src={"./contact.svg"} width={780} height={800} alt="" />
+    </div>
+  </div>
+);
