603f70cfd93e4b6be3573b6d5a0268c3c07a4286
Claudio Tomas <ccaducarlos@gmail.com>
2024-09-20 00:33:17 +0200

    fix: bug total pages

--- src/app/_components/cards/project-card.tsx
+++ src/app/_components/cards/project-card.tsx
@@ -1,9 +1,24 @@
+'use client'
+
 import Image from 'next/image'
 import { CardProps } from './CardProps'
+import { Button } from '../button'
+import { ArrowRight } from '@phosphor-icons/react'
 
+const truncateText = (text: string, maxLength: number) => {
+  if (text.length <= maxLength) return text;
+  return text.substring(0, maxLength) + '...';
+}
 
 export const ProjectCard = ({ title, text, imgSrc }: CardProps) => {
+  const truncatedText = truncateText(text ?? '', 100);
+
   return (
-    <div><Image src={"./project1.svg"} alt='' width={490} height={460}/></div>
+    <div className='flex flex-col gap-[0.6254rem]'>
+      <Image src={"./project1.svg"} alt='' width={490} height={460} />
+      <h3 className='text-xl font-medium leading-[1.8rem] text-slate-arc-700 '>{title}</h3>
+      <p className='text-base font-normal leading-6 text-slate-arc-600 '>{truncatedText}</p>
+      <Button content='Ver mais sobre o projecto' icon={<ArrowRight/>}/>
+    </div>
   )
 }
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,8 @@
 import { Footer } from "@/app/_components/footer";
 import { Navbar } from "@/app/_components/navbar";
 import { SectionHeader } from "@/app/_components/section-header";
+import { ProjectCard } from "../_components/cards/project-card";
+import {Pagination, PaginationItem, PaginationCursor} from "@nextui-org/pagination";
 
 export default function Projects() {
   return (
@@ -11,8 +13,16 @@ export default function Projects() {
         </div>
         <main className="mt-11 max-w-7xl flex flex-col gap-32 ">
           <SectionHeader title="Nossos Projectos" />
-
+          <div className="grid grid-cols-3 gap-4">
+            <ProjectCard imgSrc="./project1.svg" title="Escola Primária 12 de Outubro" text="A Escola Primária 12 de Outubro foi concebida com o objetivo de proporcionar uma educação de qualidade para crianças em idade escolar na comunidade local. O processo de criação do projeto envolveu várias etapas importantes, desde a concepção da ideia inicial até a implementação final."/>
+            <ProjectCard imgSrc="./project1.svg" title="Escola Primária 12 de Outubro" text="A Escola Primária 12 de Outubro foi concebida com o objetivo de proporcionar uma educação de qualidade para crianças em idade escolar na comunidade local. O processo de criação do projeto envolveu várias etapas importantes, desde a concepção da ideia inicial até a implementação final."/>
+            <ProjectCard imgSrc="./project1.svg" title="Escola Primária 12 de Outubro" text="A Escola Primária 12 de Outubro foi concebida com o objetivo de proporcionar uma educação de qualidade para crianças em idade escolar na comunidade local. O processo de criação do projeto envolveu várias etapas importantes, desde a concepção da ideia inicial até a implementação final."/>
+            <ProjectCard imgSrc="./project1.svg" title="Escola Primária 12 de Outubro" text="A Escola Primária 12 de Outubro foi concebida com o objetivo de proporcionar uma educação de qualidade para crianças em idade escolar na comunidade local. O processo de criação do projeto envolveu várias etapas importantes, desde a concepção da ideia inicial até a implementação final."/>
+            <ProjectCard imgSrc="./project1.svg" title="Escola Primária 12 de Outubro" text="A Escola Primária 12 de Outubro foi concebida com o objetivo de proporcionar uma educação de qualidade para crianças em idade escolar na comunidade local. O processo de criação do projeto envolveu várias etapas importantes, desde a concepção da ideia inicial até a implementação final."/>
+            <ProjectCard imgSrc="./project1.svg" title="Escola Primária 12 de Outubro" text="A Escola Primária 12 de Outubro foi concebida com o objetivo de proporcionar uma educação de qualidade para crianças em idade escolar na comunidade local. O processo de criação do projeto envolveu várias etapas importantes, desde a concepção da ideia inicial até a implementação final."/>
+          </div>
         </main>
+        {/* <Pagination totalPages={10} initialPage={1} /> */}
       </div>
       <div>
         <Footer />
