d76914154283d074bb4f70a81cad14c9c3341848
Claudio Tomas <ccaducarlos@gmail.com>
2024-09-21 15:49:50 +0200

    refactor: update image paths and styles in section-header and project-card components

--- src/app/_components/cards/project-card.tsx
+++ src/app/_components/cards/project-card.tsx
@@ -17,14 +17,16 @@ const truncateText = (text: string, maxLength: number) => {
 
 export const ProjectCard = ({ title, text, imgSrc, id }: ProjectCardProps) => {
   const truncatedText = truncateText(text ?? '', 100);
-
+  console.log(imgSrc)
   return (
-    <div className='flex flex-col gap-[0.6254rem] p-3 hover:shadow-lg hover:border border-slate-arc-300'>
-      <Image src={"./project1.svg"} alt='' width={490} height={460} />
+    <div className='flex flex-col gap-[0.6254rem] p-3 hover:shadow-lg hover:border border-slate-arc-300 rounded-2xl'>
+      <Image src={imgSrc ?? ""} alt='' width={490} height={460} />
+      
       <h3 className='text-xl font-medium leading-[1.8rem] text-slate-arc-700 '>{title}</h3>
       <p className='text-base font-normal leading-6 text-slate-arc-600 '>{truncatedText}</p>
-      <div><Button content='Ver mais sobre o projecto' icon={<ArrowRight/>}/></div>
-      <Link  href={`/projects/${id}`}>Ver mais sobre o projecto</Link>
+      <div>
+        <Link className='bg-blue-arc-700 hover:bg-blue-arc-800 text-white transition-colors duration-200  py-2 px-4 flex justify-center items-center gap-2 rounded-3xl max-w-60'  href={`/projects/${id}`}>Ver mais sobre o projecto <ArrowRight/> </Link>
+      </div>
     </div>
   )
 }
--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,7 +1,9 @@
-import { notFound } from 'next/navigation';
-import { Navbar } from '@/app/_components/navbar';
 import { Footer } from '@/app/_components/footer';
-import projectsData from '@/data/projects.json'; 
+import { Navbar } from '@/app/_components/navbar';
+import { SectionHeader } from '@/app/_components/section-header';
+import projectsData from '@/data/projects.json';
+import Image from 'next/image';
+import { notFound } from 'next/navigation';
 
 interface Project {
   id: string;
@@ -33,10 +35,42 @@ export default function ProjectDetails({ params }: ProjectProps) {
         <div className="px-20 pt-4">
           <Navbar />
         </div>
-        <main className="mt-11 max-w-7xl flex flex-col">
-          <h1 className="text-3xl font-bold">{project.title}</h1>
-          <img src={project.imgSrc} alt={project.title} className="w-full h-auto" />
-          <p className="mt-4 text-lg">{project.text}</p>
+        <main className="mt-11 max-w-7xl ">
+          <div className='flex gap-8'>
+            <div className='w-[50rem] '>
+              <h1 className="text-3xl font-bold text-blue-arc-900">{project.title}</h1>
+              <Image src={project.imgSrc} width={800} height={512} alt={project.title} />
+            </div>
+            <div className='flex flex-col w-[48rem] gap-8'>
+              <div>
+                <h2 className='font-bold leading-7 text-xl'>Introdução</h2>
+                {/* <p className="mt-4 text-lg">{project.text}</p> */}
+                <p className='text-xl leading-7'>A criação da Escola Primária 12 de Outubro foi um processo estratégico que visou atender à necessidade de educação de qualidade na comunidade local. O projeto envolveu uma série de etapas fundamentais, desde a identificação da necessidade até a implementação e abertura da escola. Abaixo, está o percurso detalhado que levou à concretização deste importante projeto educacional.</p>
+              </div>
+              <div>
+                <h2 className='font-bold leading-7 text-xl'>Identificação da Necessidade de Educação</h2>
+                <p className='text-xl leading-7'>A criação da Escola Primária 12 de Outubro foi um processo estratégico que visou atender à necessidade de educação de qualidade na comunidade local. O projeto envolveu uma série de etapas fundamentais, desde a identificação da necessidade até a implementação e abertura da escola. Abaixo, está o percurso detalhado que levou à concretização deste importante projeto educacional.</p>
+              </div>
+              <div>
+                <h2 className='font-bold leading-7 text-xl'>Planejamento do Projeto</h2>
+                <p className='text-xl leading-7'>Após a identificação da necessidade, foi criado um plano detalhado para a criação da escola. Esse planejamento envolveu a definição clara da missão e da visão da instituição, que se centraram na formação de crianças com uma base educacional sólida e valores éticos. Além disso, foram feitos estudos para determinar o número de turmas, o currículo a ser seguido, os recursos didáticos necessários, e a estrutura física ideal para abrigar as atividades escolares. O plano também incluiu a elaboração de um cronograma para a construção e implementação gradual das atividades.</p>
+              </div>
+            </div>
+          </div>
+          <aside className='gap-10 mb-24'>
+            <SectionHeader title="Galeria" />
+            <div className="grid grid-cols-2 grid-rows-2 gap-4">
+              <div className="col-span-1 row-span-1">
+                <Image src="/galeria1.svg" width={788} height={522} alt="Image 1" />
+              </div>
+              <div className="col-span-1 row-span-1">
+                <Image src="/galeria1.svg"  width={788} height={522} alt="Image 2" />
+              </div>
+              <div className="col-span-2 row-span-1">
+                <Image src="/galeria2.svg" width={1600} height={522} alt="Image 3" />
+              </div>
+            </div>
+          </aside>
         </main>
       </div>
       <div>
