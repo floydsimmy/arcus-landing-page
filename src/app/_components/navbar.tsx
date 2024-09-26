cd65ab9b86c233bcbbf1c9e00cfe6dc5c8c22a4d
Claudio Tomas <ccaducarlos@gmail.com>
2024-09-26 23:40:22 +0200

    style: navbar

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -25,10 +25,9 @@ const menuItems = [
 export const Navbar = () => {
   const [open, setOpen] = useState(false);
   return (
-    <nav className="flex fixed z-20 lg:top-4 gap-64 pr-[1.375rem]  max-w-[80rem] items-center border border-blue-arc-100 lg:rounded-[64px] bg-white">
+    <nav className="flex fixed z-20 lg:top-4 gap-64 pr-[1.375rem]  max-w-[80rem] items-center border bg-slate-700 border-blue-arc-100 lg:rounded-[64px] max-md:h-12 max-md:px-3">
       <div className="logo flex items-center">
-
-        <Image src="/logo.png" className="" alt="Logo" width={150} height={150} />
+        <Image src="/logo.png" className="max-md:scale-150" alt="Logo" width={150} height={150} />
         {/*  <span className="text-lg text-slate-700 leading-7 font-medium" >ARCUS</span> */}
       </div>
       <ul className={`list-none flex gap-8 max-md:absolute transition-all ease-in max-md:z-[-1] max-md:flex-col max-md:h-screen ${open ? "top-11 opacity-100 pt-8 pb-6 px-4" : "top-[-690px]"} bg-white max-md:w-full `}>
@@ -47,9 +46,9 @@ export const Navbar = () => {
       <button className="lg:hidden" onClick={() => setOpen(!open)}>
       {
           open ? (
-            <X weight="bold" className="lg:hidden"/>
+            <X weight="bold" size={30} className="lg:hidden"/>
           ) : (
-            <List weight="bold" className="lg:hidden" />
+            <List weight="bold" size={30} className="lg:hidden" />
           )
         }
       </button>
