45376042c2696c97ed435f62feffdf1e1e9c24d3
Claudio Tomas <ccaducarlos@gmail.com>
2024-09-28 13:57:18 +0200

    style: update navbar background transparency and add dynamic background color

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -25,12 +25,12 @@ const menuItems = [
 export const Navbar = () => {
   const [open, setOpen] = useState(false);
   return (
-    <nav className="flex fixed z-20 lg:top-4 gap-64 lg:pr-[1.375rem] lg:max-w-[80rem] items-center border-b lg:border border-blue-arc-100 bg-white/85 backdrop-blur-md lg:rounded-[64px] h-20">
+    <nav className={`flex fixed z-20 lg:top-4 gap-64 lg:pr-[1.375rem] lg:max-w-[80rem] items-center border-b lg:border border-blue-arc-100 ${open ? "bg-white" : "bg-white/85" }  backdrop-blur-md lg:rounded-[64px] h-20`}>
       <div className="logo max-md:ml-3 flex items-center">
         <Image src="/logo.png" className="max-md:scale-150" alt="Logo" width={150} height={150} />
         {/*  <span className="text-lg text-slate-700 leading-7 font-medium" >ARCUS</span> */}
       </div>
-      <ul className={`list-none flex gap-8 max-md:absolute transition-all ease-in max-md:z-[-1] max-md:flex-col max-md:h-screen ${open ? "top-16 opacity-100 pt-8 pb-6 px-4" : "top-[-990px]"} bg-white max-md:w-full `}>
+      <ul className={`list-none flex gap-8 max-md:absolute transition-all ease-in max-md:z-[-1] max-md:flex-col max-md:h-screen ${open ? "top-16 opacity-100 pt-8 pb-6 px-4 bg-white backdrop-blur-md" : "top-[-990px]"} max-md:w-full`}>
         {menuItems.map((item, index) => (
           <motion.li
             key={index}
