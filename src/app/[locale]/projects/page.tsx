a424f97668caa13ddd6b02532d3746d57c231997
Claudio Tomas <ccaducarlos@gmail.com>
2024-09-30 04:33:36 +0200

    feat: projects page translate

--- src/app/[locale]/projects/page.tsx
+++ src/app/[locale]/projects/page.tsx
@@ -1,17 +1,20 @@
 import { Footer } from '@/app/_components/footer'
 import { Navbar } from '@/app/_components/navbar'
 import { SectionHeader } from '@/app/_components/section-header'
-import projectsData from '@/data/projects.json'
+// import projectsData from '@/data/projects.json'
 import { Pagination } from '@nextui-org/pagination'
 import { ProjectCard } from '../../_components/cards/project-card'
+import { useTranslations } from 'next-intl'
 
 export default function Projects() {
+  const t = useTranslations('Projects')
+  const projectsData = t.raw(`ProjectList`)
   return (
     <div>
       <div className="flex flex-col items-center h-full my-0 bg-white">
         <Navbar />
         <main className="mt-28 max-w-7xl px-4 sm:px-9 flex flex-col">
-          <SectionHeader title="Nossos Projectos" />
+          <SectionHeader title={t(`SectionHeader.title`)} />
 
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 sm:gap-12 mt-8">
             {projectsData.map((project) => (
